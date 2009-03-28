91f40d75ee851e4e00b9a8ba4ddfe8112495c947
Greg Stoll <greg@gregstoll.com>
2009-03-28 19:53:58 +0000

    adding some marriage data
    git-svn-id: file:///usr/local/svnrepository/gaymarriagemap@2414 1105561e-45e8-0310-ac4d-b20c3b09defe

---
+++ marriagedata.js
@@ -0,0 +1,65 @@
+// Code:
+// Mar - same-sex marriage allowed
+// CU - same-sex civil unions allowed
+// CULite - some same-sex rights
+// None - none/unclear
+// NoMar - no same-sex marriage by statute
+// NoCU - no same-sex civil unions by statute
+// NoMarConst - no same-sex marriage in constitution
+// NoCUConst - no same-sex civil unions in constitution
+// References:
+// http://marriagelaw.cua.edu/law/states/AL.cfm
+// http://www.domawatch.org/stateissues/alaska/index.html
+{
+'AL':[['1998-05', 'NoMar', 'The <a href="http://www.legislature.state.al.us/CodeofAlabama/1975/30-1-19.htm">Marriage Protection Act</a> (Act 98-500) is passed, forbidding same-sex marriage.'], ['2006-06', 'NoCUConst', '<a href="http://www.legislature.state.al.us/CodeofAlabama/Constitution/1901/CA-1470311.htm">Amendment 774</a> to the Alabama constitution is passed, forbidding marriage or other "unions replicating marriage".']],
+'AK':[['1998-02', 'Mar', 'In <a href="http://www.marriagewatch.org/Law/cases/brause.cfm">Brause v. Bureau of Vital Statistics</a>, an Alaska Superior Judge ruled choosing a marital partner is a fundamental right that cannot be denied by the state without a compelling reason.'], ['1998-11', 'NoMarConst', '<a href="http://ltgov.state.ak.us/constitution.php?section=1">Amendment to the Alaska Constitution is passed (Article 1, Section 25), forbidding same-sex marriage.']],
+'AZ':[['1975-01', 'NoMar', 'A <a href="http://www.buddybuddy.com/t-line-1.html#pro-75">marriage license was given to two Phoenix men.  The state legislature then passed an emergency bill banning same-sex marriage.'], ['2008-11', 'NoMarConst', 'Proposition 102 passed, forbidding same-sex marriage.']],
+'AR':[['2004-11', 'NoCUConst', '<a href="http://www.sos.arkansas.gov/ar-constitution/arcamend83/arcamend83.htm">Amendment 83 to the Arkansas Constitution</a> was passed, forbidding same-sex marriage or legal status "which is identical or substantially similar to marital status".]],
+'CA':[['1999-09', 'CULite', '<a href="http://www.leginfo.ca.gov/cgi-bin/displaycode?section=fam&group=00001-01000&file=297-297.5">Assembly Bill 26 of 1999</a> passed, granting domestic partners <a href="http://en.wikipedia.org/wiki/Domestic_partnership_in_California#Assembly_Bill_26_of_1999">some rights</a>.], ['2000-03', 'CULite', '<a href="http://en.wikipedia.org/wiki/California_Proposition_22_(2000)">Proposition 22</a> was passed, forbidding same-sex marriage.'], ['2005-06', 'CU', '<a href="http://en.wikipedia.org/wiki/Domestic_partnership_in_California#Subsequent_changes_and_clarifications'>Domestic partner rights were expanded</a> to most of the rights that married couples have.'], ['2008-06', 'Mar', 'The California Supreme Court ruled <a href="http://en.wikipedia.org/wiki/In_re_Marriage_Cases">In re Marriage Cases</a> that the previous statutory same-sex marriage ban was unconstitutional.'], ['2008-11', 'CU', '<a href="http://en.wikipedia.org/wiki/California_Proposition_8_(2008)">Proposition 8</a> passed, amending the state constitution to forbid same-sex marriage.]],
+'CO':[['1975-01', 'NoMar', 'After the county clerk of Boulder County allowed six same-sex couples to wed, a law was quickly pushed through to forbid same-sex marriage'], ['2006-11', 'NoMarConst', '<a href="http://www.domawatch.org/stateissues/colorado/index.html">Amendment #43</a> to the Colorado constitution passed, forbidding same-sex marriage.']],
+'CT':[['2005-04', 'CU', 'The state legislature passed <a href="http://www.cga.ct.gov/2005/act/Pa/2005PA-00010-R00SB-00963-PA.htm">Public Act No. 05-10</a>, establishing same-sex civil unions.'], ['2008-10', 'Mar', 'The Connecticut supreme court ruled in <a href="http://en.wikipedia.org/wiki/Kerrigan_v._Commissioner_of_Public_Health">Kerrigan v. Commissioner of Public Health</a> that the constitution protects the right of same-sex marriage.']],
+'DC':[],
+'DE':[],
+'FL':[],
+'GA':[],
+'HI':[],
+'ID':[],
+'IL':[],
+'IN':[],
+'IA':[],
+'KS':[],
+'KY':[],
+'LA':[],
+'ME':[],
+'MD':[],
+'MA':[],
+'MI':[],
+'MN':[],
+'MS':[],
+'MO':[],
+'MT':[],
+'NE':[],
+'NV':[],
+'NH':[],
+'NJ':[],
+'NM':[],
+'NY':[],
+'NC':[],
+'ND':[],
+'OH':[],
+'OK':[],
+'OR':[],
+'PA':[],
+'RI':[],
+'SC':[],
+'SD':[],
+'TN':[],
+'TX':[],
+'UT':[],
+'VT':[],
+'VA':[],
+'WA':[],
+'WV':[],
+'WI':[],
+'WY':[]
+};

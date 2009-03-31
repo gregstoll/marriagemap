639fdf8628b8fa932d06e003f9d8ee5785a9bc34
Greg Stoll <greg@gregstoll.com>
2009-03-31 02:56:36 +0000

    displaying legend, doing mouseover titles and showing state data on click, clickable dates
    git-svn-id: file:///usr/local/svnrepository/gaymarriagemap@2420 1105561e-45e8-0310-ac4d-b20c3b09defe

--- processareadata.py
+++ processareadata.py
@@ -2,13 +2,13 @@
 
 import sys, os, re
 
-f = open('areadata.txt', 'r')
+f = open('newareadata.txt', 'r')
 areaRe = re.compile('coords="(.*?)"')
 for l in f.readlines():
     areaMatch = areaRe.search(l)
     if areaMatch:
         match = areaMatch.group(1)
-        matchCoords = [str(2*int(x)) for x in match.split(',')]
+        matchCoords = [str(int(x) + (int(x)/50)) for x in match.split(',')]
         print l[:areaMatch.start(0)] + 'coords="' + ','.join(matchCoords) + '"' + l[areaMatch.end(0):-1]
 
 
